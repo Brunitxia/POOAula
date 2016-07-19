0333637e8c3ce58c00f85b5dee420b540e26acf2
Brunitxia <brunitxia.rafaella@yahoo.com>
2016-07-19 17:26:29 -0300

    Add files via upload
    lista de função

---
+++ lista01.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+void imprimi_numero();
+int main()
+{
+/*
+Programa IMPRIMIR VALORES.
+*/
+imprimi_numero();
+// O bloco termina aqui.
+}
+void imprimi_numero()
+{for (int cont = 1; cont <= 100; cont++)
+{
+if (cont % 2 == 0) cout << cont << endl;
+}
+ for (int cont = 1; cont <= 100; cont++)
+{
+if (cont % 2 == 0) cout << cont << endl;
+}
+ 
+}
+
+
+ 
